d29c8dad940d64fcc7791d14cb9a858c9195495e
Patrycja Kilian <patrycjkilian0@gmail.com>
2025-04-23 18:26:25 +0200

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Patrycja");
+        System.out.println(25);
+        System.out.println('K');
+    }
+}

7153f8c0e2c9ef73834972b4636816ebbfb28e5c
Patrycja Kilian <patrycjkilian0@gmail.com>
2025-04-27 18:25:55 +0200

    Instukcje warunkowe

--- kodilla-intro/src/main/java/DubugExample.java
+++ kodilla-intro/src/main/java/DubugExample.java
@@ -1,16 +1,26 @@
 public class DubugExample {
     public static void main(String[] args) {
-        int firstNumber = 11;
+        int firstNumber = 23;
         int secondNumber = 22;
 
         System.out.println(firstNumber);
         System.out.println(secondNumber);
 
-        sumAndDisplay(firstNumber, secondNumber);
+        if (firstNumber > secondNumber) {
+            subtractAndDisplay(firstNumber, secondNumber);
+        } else {
+            sumAndDisplay(firstNumber, secondNumber);
+        }
     }
 
     private static void sumAndDisplay(int a, int b) {
         int result = a + b;
+
+        System.out.println(result);
+    }
+
+    private static void subtractAndDisplay(int a, int b) {
+        int result = a - b;
 
         System.out.println(result);
     }

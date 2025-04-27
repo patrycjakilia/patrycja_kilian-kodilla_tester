53afc8ee5a07fc366eea870e6a2ab82f9dc9f97e
Patrycja Kilian <patrycjkilian0@gmail.com>
2025-04-27 17:55:55 +0200

    Main Class - zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        int age = 1;
+        String name = "Patrycja";
+        char letter = 'P';
+        String profession = "tester";
+        double pointNumber = 1.123;
+        boolean value = true;
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(letter);
+        System.out.println(profession);
+        System.out.println(pointNumber);
+        System.out.println(value);
+    }
+
+}

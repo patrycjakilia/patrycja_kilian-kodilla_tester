52c759b4002855e42bb39f6ee79476e1674bbbf4
Patrycja Kilian <patrycjkilian0@gmail.com>
2025-05-18 09:20:16 +0200

    Application

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,5 +1,4 @@
 public class Application {
-
     public static void main(String[] args) {
         Users users = new Users("Adam", 40.5,178);
         UserValidator userValidator = new UserValidator();

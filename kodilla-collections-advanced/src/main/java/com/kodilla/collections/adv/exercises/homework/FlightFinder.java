cbf7f5f9467f8f244bdf5700322e9360bc2ebc2c
pkilian <patrycjakilian0@gmail.com>
2025-06-06 15:22:52 +0200

    Flight

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+public class FlightFinder {
+    public List<Flight> findFlightsFrom(String departure){
+
+    }
+    public List<Flight> findFlightsTo(String arrival){
+
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepositor.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import java.util.List;
+
+public class FlightRepositor {
+
+    public static List<Flight> getFlightsTable(){
+
+    }
+}

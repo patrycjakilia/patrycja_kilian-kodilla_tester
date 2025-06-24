4b8900b56701cead4e0760bca443bac3e90578fa
pkilian <patrycjakilian0@gmail.com>
2025-06-24 14:12:11 +0200

    Order

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/Shop.java
@@ -0,0 +1,29 @@
+package com.kodilla.execution_model.homework;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Shop {
+    Collection<Order> purchases = new HashSet<>();
+
+    public void addOrder(Order order){
+
+    }
+
+    public void getOrderByDate(LocalDate firstDate, LocalDate secondDate){
+
+    }
+
+    public void getOrderByValueOfPurchase(double firstValueOfPurchase, double secondValueOfPurchase){
+
+    }
+
+    public void getCountOfOrder(){
+
+    }
+
+    public void sumAllOrders(){
+
+    }
+}

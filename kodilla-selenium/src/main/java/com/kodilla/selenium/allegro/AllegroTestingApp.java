89932fec7c993c830e5865018f0236177e36d759
Patrycja Kilian <patrycjkilian0@gmail.com>
2025-08-29 12:50:58 +0200

    ALlegro

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class AllegroTestingApp {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/patry/IdeaProjects/patrycja_kilian-kodilla_tester/chromedriver-win64/chromedriver-win64/chromedriver.exe");  	// [1]
@@ -21,17 +23,27 @@ public class AllegroTestingApp {
 
         WebDriverWait wait = new WebDriverWait(webDriver, 20);
 
-        WebElement acceptCookies = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-role='accept-consent']")));
+        WebElement acceptCookies = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[data-role='accept-consent']")));
         acceptCookies.click();
 
-        WebElement categoryCombo = webDriver.findElement(By.xpath("//select[@data-role='filters-dropdown']"));
+        WebElement categoryCombo = webDriver.findElement(By.cssSelector("select[data-role='search-scope-select']"));
         Select categorySelect = new Select(categoryCombo);
         categorySelect.selectByIndex(4);
 
-        WebElement searchField = webDriver.findElement( By.xpath("//input[@type='search']"));
+        WebElement searchField = webDriver.findElement( By.cssSelector("input[type='search']"));
         searchField.sendKeys("Mavic min");
 
-        WebElement searchButton = webDriver.findElement(By.xpath("//button[@type='submit']"));
+        WebElement searchButton = webDriver.findElement(By.cssSelector("button[type='submit']"));
         searchButton.click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("section article")));
+        List<WebElement> element = webDriver.findElements(By.cssSelector("section article"));
+
+        List<WebElement> products = webDriver.findElements(By.cssSelector("section > article"));
+
+        for (WebElement product : products) {
+            System.out.println(product.getText());
+        }
+
     }
 }

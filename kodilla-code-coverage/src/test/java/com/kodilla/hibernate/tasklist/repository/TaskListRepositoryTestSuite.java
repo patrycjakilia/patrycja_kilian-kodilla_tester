db2115f347b36b1962dd151473cf3a207a52231b
Patrycja Kilian <patrycjkilian0@gmail.com>
2025-09-07 10:28:30 +0200

    hibernate

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -0,0 +1,39 @@
+package com.kodilla.hibernate.tasklist.repository;
+
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.repository.TaskRepository;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+
+@SpringBootTest
+class TaskListRepositoryTestSuite {
+
+    @Autowired
+    private TaskListRepository taskListRepository;
+
+    @Test
+    void testFindByListName(){
+        //Given
+        TaskList taskList = new TaskList("ToDo", "Tasks to do");
+        taskListRepository.save(taskList);
+
+        //When
+        List<TaskList> result = taskListRepository.findByListName("ToDo");
+
+        //Then
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals("Tasks to do", result.get(0).getDescription());
+
+        //CleanUp
+        taskListRepository.deleteAll();
+
+    }
+
+}
